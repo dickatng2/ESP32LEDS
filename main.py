d091e9f215e5a19ac3b431acbf634eec566eaba8
dickatng2 <39006101+dickatng2@users.noreply.github.com>
2024-10-24 17:06:19 +0200

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # test
 import machine, time
 from machine import Pin, PWM
+from time import sleep
 led23 = Pin(23, Pin.OUT)
 pwm = machine.PWM(led23)
 t_slp = 1
 while True:
   pwm.duty(1023)
   time.sleep(t_slp)
+  pwm.duty(700)
+  time.sleep(t_slp)
   pwm.duty(500)
   time.sleep(t_slp)
-  
+  pwm.duty(200)
+  time.sleep(t_slp)
