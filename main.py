6fc8a240ef99a5587bc43ac55e57d8dfc1623425
dickatng2 <39006101+dickatng2@users.noreply.github.com>
2024-10-24 16:54:14 +0200

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # test
-print ("test ota")
+import machine, time
+from machine import Pin, PWM
+led23 = Pin(23, Pin,OUT)
+pwm = machine.PWM(led23)
+t_slp = 1
+while True:
+  pwm.duty(1023)
+  time.sleep(t_slp)
+  pwm.duty(500)
+  time.sleep(t_slp)
+  
