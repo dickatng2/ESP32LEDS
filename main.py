db2cacc02e508a31562ecff644390cdcd6ff9d8c
dickatng2 <39006101+dickatng2@users.noreply.github.com>
2024-10-24 18:10:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from time import sleep
 led23 = Pin(23, Pin.OUT)
 pwm = machine.PWM(led23)
 t_slp = 1
+def timer_callback:
+  test_ota.py
+
+my_timer = Timer(id)
+my_timer.init(mode=Timer.PERIODIC, period=100000, callback=timer_callback)
+
 while True:
   pwm.duty(1023)
   time.sleep(t_slp)
