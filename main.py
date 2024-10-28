1e8df61134bda01e0c499d3d43357ec91ae791d4
dickatng2 <39006101+dickatng2@users.noreply.github.com>
2024-10-28 15:46:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ import os
 import json
 import ntptime
 
-led = []
-for i in range (23):
-    led.append(machine.PWM(Pin(i,Pin.OUT)))
+#led = []
+#for i in range (23):
+#    led.append(machine.PWM(Pin(i,Pin.OUT)))
+pwm23 = machine.PWM(Pin(23,Pin.OUT))
 
-from ota import OTAUpdater
-from WIFI_CONFIG import SSID, PASSWORD
+
+#from ota import OTAUpdater
+from wifi_config import SSID, PASSWORD
 firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+#ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 
-led[0].init
+#led[0].init
 
 
 def timer_callback():
@@ -28,13 +30,14 @@ def timer_callback():
 # 
 #     firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
 #     ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-    ota_updater.download_and_install_update_if_available()
+    print("callback")
+    #ota_updater.download_and_install_update_if_available()
 
 def timer_test(a):
     timer_callback()
 
 def licht01():
-  t_slp = 0.5
+  t_slp = 0.3
   pwm23.duty(1023)
   time.sleep(t_slp)
   pwm23.duty(700)
@@ -51,22 +54,25 @@ def licht01():
   time.sleep(t_slp)
   pwm23.duty(0)
   time.sleep(t_slp)
-  pwm22.duty(1023)
-  time.sleep(t_slp)
-  pwm22.duty(700)
-  time.sleep(t_slp)
-  pwm22.duty(500)
-  time.sleep(t_slp)
-  pwm22.duty(200)
-  time.sleep(t_slp)
-  pwm22.duty(100)
-  time.sleep(t_slp)
-  pwm22.duty(50)
-  time.sleep(t_slp)
-  pwm22.duty(10)
-  time.sleep(t_slp)
-  pwm22.duty(0)
-  time.sleep(t_slp)
+#   pwm22.duty(1023)
+#   time.sleep(t_slp)
+#   pwm22.duty(700)
+#   time.sleep(t_slp)
+#   pwm22.duty(500)
+#   time.sleep(t_slp)
+#   pwm22.duty(200)
+#   time.sleep(t_slp)
+#   pwm22.duty(100)
+#   time.sleep(t_slp)
+#   pwm22.duty(50)
+#   time.sleep(t_slp)
+#   pwm22.duty(10)
+#   time.sleep(t_slp)
+#   pwm22.duty(0)
+#   time.sleep(t_slp)
         
 my_timer = Timer(0)
 my_timer.init(mode=Timer.PERIODIC, period=20000, callback=timer_test)
+while True:
+    licht01()
+    
