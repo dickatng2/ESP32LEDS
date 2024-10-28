06b38a09f3998af3db12a67f575893f522675f28
dickatng2 <39006101+dickatng2@users.noreply.github.com>
2024-10-28 13:15:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,59 +2,71 @@
 import machine, time
 from machine import Pin, PWM, Timer
 from time import sleep
-led23 = Pin(23, Pin.OUT)
-led22 = Pin(22, Pin.OUT)
-pwm23 = machine.PWM(led23)
-pwm22 = machine.PWM(led22)
-t_slp = 1
+import sys
+import network
+import urequests
+import os
+import json
+import ntptime
+
+led = []
+for i in range (23):
+    led.append(machine.PWM(Pin(i,Pin.OUT)))
+
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+
+led[0].init
+
 
 def timer_callback():
-    from ota import OTAUpdater
-    from WIFI_CONFIG import SSID, PASSWORD
-
-    firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
-    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+#     from ota import OTAUpdater
+#     from WIFI_CONFIG import SSID, PASSWORD
+# 
+#     firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
+#     ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
     ota_updater.download_and_install_update_if_available()
 
 def timer_test(a):
     timer_callback()
 
 def licht01():
-    while True:
-      pwm23.duty(1023)
-      time.sleep(t_slp)
-      pwm23.duty(700)
-      time.sleep(t_slp)
-      pwm23.duty(500)
-      time.sleep(t_slp)
-      pwm23.duty(200)
-      time.sleep(t_slp)
-      pwm23.duty(100)
-      time.sleep(t_slp)
-      pwm23.duty(50)
-      time.sleep(t_slp)
-      pwm23.duty(10)
-      time.sleep(t_slp)
-      pwm23.duty(5)
-      time.sleep(t_slp)
-
-pwm23.duty(1023)
-      time.sleep(t_slp)
-      pwm22.duty(700)
-      time.sleep(t_slp)
-      pwm22.duty(500)
-      time.sleep(t_slp)
-      pwm22.duty(200)
-      time.sleep(t_slp)
-      pwm22.duty(100)
-      time.sleep(t_slp)
-      pwm22.duty(50)
-      time.sleep(t_slp)
-      pwm22.duty(10)
-      time.sleep(t_slp)
-      pwm22.duty(5)
-      time.sleep(t_slp)
+  t_slp = 0.5
+  pwm23.duty(1023)
+  time.sleep(t_slp)
+  pwm23.duty(700)
+  time.sleep(t_slp)
+  pwm23.duty(500)
+  time.sleep(t_slp)
+  pwm23.duty(200)
+  time.sleep(t_slp)
+  pwm23.duty(100)
+  time.sleep(t_slp)
+  pwm23.duty(50)
+  time.sleep(t_slp)
+  pwm23.duty(10)
+  time.sleep(t_slp)
+  pwm23.duty(0)
+  time.sleep(t_slp)
+  pwm22.duty(1023)
+  time.sleep(t_slp)
+  pwm22.duty(700)
+  time.sleep(t_slp)
+  pwm22.duty(500)
+  time.sleep(t_slp)
+  pwm22.duty(200)
+  time.sleep(t_slp)
+  pwm22.duty(100)
+  time.sleep(t_slp)
+  pwm22.duty(50)
+  time.sleep(t_slp)
+  pwm22.duty(10)
+  time.sleep(t_slp)
+  pwm22.duty(0)
+  time.sleep(t_slp)
         
-my_timer = Timer(1)
-my_timer.init(mode=Timer.PERIODIC, period=200000, callback=timer_test)
-licht01()
+my_timer = Timer(0)
+my_timer.init(mode=Timer.PERIODIC, period=20000, callback=timer_test)
