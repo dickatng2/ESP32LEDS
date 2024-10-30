30ba2f1510470542b0e80e0ba086688614d0db8d
dickatng2 <39006101+dickatng2@users.noreply.github.com>
2024-10-30 19:53:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,32 @@ from ota import OTAUpdater
 from wifi_config import SSID, PASSWORD
 firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-pwm23 = machine.PWM(Pin(23,Pin.OUT))
+#ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 
-led= []
-led.append(pwm23)
-leng = len(led)
+pwm = [22,23,21,19,27,12,14,26]
+len_pwm = len(pwm)
+
+duur = 0.3
+pwm[0] = machine.PWM(Pin(12, Pin.OUT))
+pwm[1] = machine.PWM(Pin(26, Pin.OUT))
+pwm[2] = machine.PWM(Pin(14, Pin.OUT))
+pwm[3] = machine.PWM(Pin(27, Pin.OUT))
+pwm[4] = machine.PWM(Pin(19, Pin.OUT))
+pwm[5] = machine.PWM(Pin(21, Pin.OUT))
+pwm[6] = machine.PWM(Pin(22, Pin.OUT))
+pwm[7] = machine.PWM(Pin(23, Pin.OUT))
+ 
+def licht04(lengte, tijd):
+    for i in range (len_pwm):
+        pwm[i].duty(300)
+        pwm[((i+1) % len_pwm)].duty(150)
+        pwm[((i+2) % len_pwm)].duty(50)
+        pwm[((i+1) % len_pwm)].duty(150)
+        pwm[((i) % len_pwm)].duty(50)
+        time.sleep(tijd)
+        pwm[i].duty(0)
+    time.sleep(0)
+        
 def timer_callback():
     from ota import OTAUpdater
     from wifi_config import SSID, PASSWORD
@@ -30,45 +50,9 @@ def timer_callback():
 
 def timer_test(a):
     timer_callback()
-    
-def licht02():
-  t_slp = 0.5
-  led[0].duty(100)
-  time.sleep(2)
-  pwm23.duty(1023)
-  time.sleep(t_slp)
-  pwm23.duty(0)
-  time.sleep(t_slp)
-
-def licht03(lengte):
-    for i in range (lengte):
-        led[i].duty(500)
-        time.sleep(1)
-        led[i].duty(0)
-        time.sleep(0.5)
-    
-def licht01():
-  t_slp = 1
-  pwm23.duty(1023)
-  time.sleep(t_slp)
-  pwm23.duty(700)
-  time.sleep(t_slp)
-  pwm23.duty(500)
-  time.sleep(t_slp)
-  pwm23.duty(200)
-  time.sleep(t_slp)
-  licht02()
-  pwm23.duty(100)
-  time.sleep(t_slp)
-  pwm23.duty(50)
-  time.sleep(t_slp)
-  pwm23.duty(10)
-  time.sleep(t_slp)
-  pwm23.duty(0)
-  time.sleep(t_slp)
-
         
 my_timer = Timer(0)
-my_timer.init(mode=Timer.PERIODIC, period=20000, callback=timer_test)
+#my_timer.init(mode=Timer.PERIODIC, period=60000, callback=timer_test)
 while True:
-    licht03(leng)
+    licht04(len_pwm,duur)
+
