d2e1c90518dc066bc517d01ad4a1a0a5ed1a4607
dickatng2 <39006101+dickatng2@users.noreply.github.com>
2024-11-16 12:38:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# version 21
+# version 26
 import machine, time
 from machine import Pin, PWM, Timer
 from time import sleep
@@ -7,52 +7,70 @@ import network
 import urequests
 import os
 import json
-import ntptime
+
+from us import HCSR04
+sensor = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=30000)
 
 from ota import OTAUpdater
-from wifi_config import SSID, PASSWORD
+from WIFI_CONFIG import SSID, PASSWORD
 firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
 
-#ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 
-pwm = [22,23,21,19,27,12,14,26]
+pwm = [22,23,21,19,27,12,10,26,25,33,32]
 len_pwm = len(pwm)
+duur = 0.1 
 
-duur = 0.3
-pwm[0] = machine.PWM(Pin(12, Pin.OUT))
-pwm[1] = machine.PWM(Pin(26, Pin.OUT))
-pwm[2] = machine.PWM(Pin(14, Pin.OUT))
-pwm[3] = machine.PWM(Pin(27, Pin.OUT))
-pwm[4] = machine.PWM(Pin(19, Pin.OUT))
-pwm[5] = machine.PWM(Pin(21, Pin.OUT))
-pwm[6] = machine.PWM(Pin(22, Pin.OUT))
-pwm[7] = machine.PWM(Pin(23, Pin.OUT))
+pwm[0] = machine.PWM(Pin(26, Pin.OUT)) 
+pwm[1] = machine.PWM(Pin(13, Pin.OUT))
+pwm[2] = machine.PWM(Pin(27, Pin.OUT))
+pwm[3] = machine.PWM(Pin(14, Pin.OUT))
+pwm[4] = machine.PWM(Pin(2, Pin.OUT))
+pwm[5] = machine.PWM(Pin(15, Pin.OUT))
+pwm[6] = machine.PWM(Pin(23, Pin.OUT))
+pwm[7] = machine.PWM(Pin(25, Pin.OUT))
+pwm[8] = machine.PWM(Pin(33, Pin.OUT))
+pwm[9] = machine.PWM(Pin(12, Pin.OUT))
+pwm[10] = machine.PWM(Pin(04, Pin.OUT))             
  
-def licht04(lengte, tijd):
-    for i in range (len_pwm):
-        pwm[i].duty(300)
-        pwm[((i+1) % len_pwm)].duty(150)
-        pwm[((i+2) % len_pwm)].duty(50)
-        pwm[((i+1) % len_pwm)].duty(150)
-        pwm[((i) % len_pwm)].duty(50)
+def licht04(lengte, tijd):                                        
+    for i in range (lengte):    
+        pwm[i].duty(1023)
+        pwm[((i+5) % len_pwm)].duty(600)
+        pwm[((i+4) % len_pwm)].duty(400)
+        pwm[((i+3) % len_pwm)].duty(100)
+        pwm[((i+2) % len_pwm)].duty(0)
+        time.sleep(tijd)
+    
+def licht05(tijd):
+    for i in range (11):
+        print (i)
+        pwm[i].freq(50)
+        pwm[i].duty(1023)
+        pwm[11-i].duty(1023)
         time.sleep(tijd)
         pwm[i].duty(0)
-    time.sleep(0)
+        pwm[11-i].duty(0)
+        wait(3)
         
 def timer_callback():
-    from ota import OTAUpdater
-    from wifi_config import SSID, PASSWORD
-
-    firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
-    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+#    firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
+#    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
     print("callback")
     ota_updater.download_and_install_update_if_available()
 
-def timer_test(a):
+def timer_test():
     timer_callback()
-        
-my_timer = Timer(0)
-#my_timer.init(mode=Timer.PERIODIC, period=60000, callback=timer_test)
-while True:
-    licht04(len_pwm,duur)
+
+def tijd():
+    my_timer = Timer(0)
+    my_timer.init(mode=Timer.PERIODIC, period=20000, callback=timer_test) 
+
+tijd()
+cnt = 0
+while True:    
+     cnt += 1
+     #licht04(len_pwm, duur)
+     print (cnt)
+     sleep(1)
 
