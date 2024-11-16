fe924f8648942ceed0062966933aa60742171bdb
dickatng2 <39006101+dickatng2@users.noreply.github.com>
2024-11-16 15:03:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# version 33
+# version 34
 import machine, time
 from machine import Pin, PWM, Timer
 from time import sleep
@@ -16,9 +16,8 @@ from WIFI_CONFIG import SSID, PASSWORD
 
 firmware_url = "https://github.com/dickatng2/ESP32LEDS/"
 my_timer = Timer(4)
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 
-pwm = [22,23,21,19,27,12,10,26,25,33,32]
+pwm = [26,13,27,14,2,15,23,25,33,12,4]
 len_pwm = len(pwm)
 duur = 0.2
 per = 60000
@@ -35,26 +34,15 @@ pwm[8] = machine.PWM(Pin(33, Pin.OUT))
 pwm[9] = machine.PWM(Pin(12, Pin.OUT))
 pwm[10] = machine.PWM(Pin(04, Pin.OUT))             
  
-def licht04(lengte, tijd):                                        
+def licht04(lengte, dt):                                        
     for i in range (lengte):    
         pwm[i].duty(1023)
         pwm[((i+5) % len_pwm)].duty(600)
         pwm[((i+4) % len_pwm)].duty(400)
         pwm[((i+3) % len_pwm)].duty(100)
         pwm[((i+2) % len_pwm)].duty(0)
-        time.sleep(tijd)
-    
-def licht05(tijd):
-    for i in range (11):
-        print (i)
-        pwm[i].freq(50)
-        pwm[i].duty(1023)
-        pwm[11-i].duty(1023)
-        time.sleep(tijd)
-        pwm[i].duty(0)
-        pwm[11-i].duty(0)
-        wait(3)
-        
+        time.sleep(dt)
+            
 def timer_test(a):
     ota_updater.download_and_install_update_if_available()
     print("callback")
@@ -63,10 +51,11 @@ def tijd():
     print ("tijd")
     my_timer.init(mode=Timer.PERIODIC, period=per, callback=timer_test) 
 
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 tijd()
 while True:    
      licht04(len_pwm, duur)
-     sleep(0.01)
+   
      
 
 
