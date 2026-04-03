14ddea01dfee430224b0069611a9771d6dd7b006
dija <39006101+dickatng2@users.noreply.github.com>
2026-04-03 19:47:08 +0200

    Fix setpoints for rel 1 and rel 2 in main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ rel_1 = Pin(26, Pin.OUT)
 rel_2 = Pin(33, Pin.OUT)
 
 rel_1_sec_on = [1,5], [14, 27] # [on/off][h/m/s] alarm setpoints rel
-rel_1_sec_off = [0, 5], [30, 46] # setpoints rel 1 uit
-rel_2_sec_on = [1,5], [15, 28] # setpoints rel 2 aan
-rel_2_sec_off = [0, 5], [30, 45] # setpoints rel 2 uit
+rel_1_sec_off = [0, 5], 17, 30] # setpoints rel 1 uit
+rel_2_sec_on = [1,5], [17, 33] # setpoints rel 2 aan
+rel_2_sec_off = [0, 5], [20, 36] # setpoints rel 2 uit
 
 rel_1_min_on = [1, 4], [15, 29, 31] # setpoints rel 1 aan
 rel_1_min_off = [0, 4], [30, 45] # setpoints rel 1 uit
